247e42f5be5acf35fe5dc40f88829736347874c7
juandjuarez <juanmajuarez@live.com.ar>
2024-10-21 14:56:12 +0000

    nuevo

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: '/src/index.js', // Asegúrate de que esta ruta sea correcta
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production', // Cambia a 'development' si es necesario
+  mode: 'production',
+  resolve: {
+    fallback: {
+      zlib: require.resolve('browserify-zlib'),
+      crypto: require.resolve('crypto-browserify'),
+      querystring: require.resolve('querystring-es3'),
+      stream: require.resolve('stream-browserify'),
+      string_decoder: require.resolve('string_decoder/'),
+      url: require.resolve('url/'),
+      buffer: require.resolve('buffer/'),
+      util: require.resolve('util/'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('stream-http'),
+      fs: false, // false si no necesitas fs
+      net: false, // false si no necesitas net
+    },
+  },
 };
